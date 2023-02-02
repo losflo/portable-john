82b6e08fa4055cb6c061e0ae14d8e83b046b7f85
losflo <30555738+losflo@users.noreply.github.com>
2023-02-01 22:14:14 -0600

    prod & test import

--- pkg/portajohn/cmd/import/test/main.go
+++ pkg/portajohn/cmd/import/test/main.go
@@ -8,8 +8,7 @@ import (
 	"os"
 	"time"
 
-	// _ "github.com/denisenkom/go-mssqldb"
-	_ "github.com/go-sql-driver/mysql"
+	_ "github.com/denisenkom/go-mssqldb"
 	"portablejohn.com/pkg/portajohn"
 )
 
@@ -32,68 +31,64 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	_, err = db.Begin()
+	err = db.Ping()
 	if err != nil {
 		panic(err)
 	}
 
 	query := `
-		SELECT 
-			c1.cocode,
-			c1.custmast,
-			c1.custnum,
-			c1.startdate,
-			c1.taxpcnt,
-			c1.taxpcnt2,
-			c1.taxexempt,
-			c1.penalty,
-			c1.po_num,
-			c1.d_waiver,
-			c1.sitename,
-			c1.siteaddr,
-			c1.siteaddr2,
-			c1.sitecity,
-			c1.sitestate,
-			c1.sitezip,
-			c1.sitephone,    
-			c1.sitefax,
-			c1.sitezip4,
-			c1.sitecntry,
-			c1.super,
+		SELECT TOP(10)
+			cocode,
+			custmast,
+			custnum,
+			startdate,
+			taxpcnt,
+			taxpcnt2,
+			taxexempt,
+			penalty,
+			po_num,
+			d_waiver,
+			sitename,
+			siteaddr,
+			siteaddr2,
+			sitecity,
+			sitestate,
+			sitezip,
+			sitephone,    
+			sitefax,
+			sitezip4,
+			sitecntry,
+			super,
 			
-			c7.bllmast,
-			c7.blladdr,
-			c7.blladdr2,
-			c7.bllcity,
-			c7.bllemail1,
-			c7.bllemail2,    
-			c7.bllfax,
-			c7.bllname,
-			c7.bllphone,
-			c7.bllstate,
-			c7.bllzip,
-			c7.bllcocode,
-			c7.bllcontact,    
-			c7.bllcountry,
+			bllmast,
+			blladdr,
+			blladdr2,
+			bllcity,
+			bllemail1,
+			bllemail2,    
+			bllfax,
+			bllname,
+			bllphone,
+			bllstate,
+			bllzip,
+			bllcocode,
+			bllcontact,    
+			bllcountry,
 										
-			c9.acctstatus,
-			c9.accttype,
-			c9.county,
-			c9.custcode1,
-			c9.terms,    
-			c9.email,
-			c9.email2,
-			c9.emaillst,
-			c9.nomail,
-			c9.nomailing,
+			acctstatus,
+			accttype,
+			county,
+			custcode1,
+			terms,    
+			email,
+			email2,
+			emaillst,
+			nomail,
+			nomailing,
 
-			c1.FKjcustmast
+			FKjcustmast
 
-		FROM jcusf01 as c1
-
-		INNER JOIN jcusf07 as c7 ON c1.FKjcustmast = c7.FKjcustmast 
-
-		INNER JOIN jcusf09 as c9 ON c1.custnum = c9.custnum
+		FROM customersites
 	`
 
 	stmt, err := db.Prepare(query)
@@ -118,52 +113,66 @@ func main() {
 			billzip, billcocode, billcontact,
 			billcountry, acctstatus, accttype,
 			county, custcode1, terms, email, email2 string
-		var custnum, taxpcnt, taxpcnt2, penalty, nomailing, fkjcustmast int
+		var custnum, taxpcnt, taxpcnt2, penalty float64
 		var emaillst, nomail bool
+		var fkjcustmast, nomailing int
 		var startdate time.Time
 
-		rows.Scan(
-			cocode,
+		err = rows.Scan(
+			&cocode,
+			&custmast,
+			&custnum,
+			&startdate,
+			&taxpcnt,
+			&taxpcnt2,
+			&taxexempt,
+			&penalty,
+			&po_num,
+			&d_waiver,
+			&sitename,
+			&siteaddr,
+			&siteaddr2,
+			&sitecity,
+			&sitestate,
+			&sitezip,
+			&sitephone,
+			&sitefax,
+			&sitezip4,
+			&sitecntry,
+			&super,
+			&billmast,
+			&billaddr,
+			&billaddr2,
+			&billcity,
+			&billemail1,
+			&billemail2,
+			&billfax,
+			&billname,
+			&billphone,
+			&billstate,
+			&billzip,
+			&billcocode,
+			&billcontact,
+			&billcountry,
+			&acctstatus,
+			&accttype,
+			&county,
+			&custcode1,
+			&terms,
+			&email,
+			&email2,
+			&emaillst,
+			&nomail,
+			&nomailing,
+			&fkjcustmast,
+		)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(
 			custmast,
-			custnum,
-			startdate,
-			taxpcnt,
-			taxpcnt2,
-			taxexempt,
-			penalty,
 			po_num,
-			d_waiver,
 			sitename,
-			siteaddr,
-			siteaddr2,
-			sitecity,
-			sitestate,
-			sitezip,
-			sitephone,
-			sitefax,
-			sitezip4,
-			sitecntry,
-			super,
-			billmast,
-			billaddr,
-			billaddr2,
-			billcity,
-			billemail1,
-			billemail2,
-			billfax,
-			billname,
-			billphone,
-			billstate,
-			billzip,
-			billcocode,
-			billcontact,
-			billcountry,
-			acctstatus,
-			accttype,
-			county,
-			custcode1,
-			terms,
-			email, email2,
 			emaillst,
 			nomail,
 			nomailing,
