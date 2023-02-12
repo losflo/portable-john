bf12898255dc77f4d5ff834e2dd9ea4784d19e3e
losflo <30555738+losflo@users.noreply.github.com>
2023-02-12 13:51:01 -0600

    import script v1

--- pkg/portajohn/cmd/import/main.go
+++ pkg/portajohn/cmd/import/main.go
@@ -10,8 +10,56 @@ import (
 
 	// _ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/go-sql-driver/mysql"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"portablejohn.com/pkg/portajohn"
 )
+
+var db *sql.DB
+var mdb *mongo.Database
+
+func init() {
+	var err error
+	qry := url.Values{}
+	qry.Add("database", "portablejohn")
+	username := os.Getenv("SQL_SERVER_USER")
+	password := os.Getenv("SQL_SERVER_PASS")
+	hostname := os.Getenv("ROSETEC_SERVER")
+	port := os.Getenv("SQL_SERVER_PORT")
+
+	u := &url.URL{
+		Scheme: "sqlserver",
+		User:   url.UserPassword(username, password),
+		Host:   fmt.Sprintf("%s:%s", hostname, port),
+		// Path:  instance, // if connecting to an instance instead of a port
+		RawQuery: qry.Encode(),
+	}
+	db, err = sql.Open("sqlserver", u.String())
+	if err != nil {
+		panic(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	dbctx, dbcancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer dbcancel()
+	opts := options.ClientOptions{}
+	opts.ApplyURI("mongodb://" + fmt.Sprintf("%s:%s", os.Getenv("ROSETEC_SERVER"), os.Getenv("MONGO_PORT")))
+	mngo, err := mongo.Connect(dbctx, &opts)
+	if err != nil {
+		panic(err)
+	}
+	err = mngo.Ping(dbctx, readpref.Primary())
+	if err != nil {
+		panic(err)
+	}
+	mdb = mngo.Database("teal_test")
+}
 
 func main() {
 	qry := url.Values{}
@@ -37,8 +85,22 @@ func main() {
 		panic(err)
 	}
 
+	// import distinct inventory and insert to mongo
+	err = importInventory()
+	if err != nil {
+		panic(err)
+	}
+
+	// import customers/sites and inventory linked to customer
+	err = importCustomerSites()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func importCustomerSites() error {
 	query := `
-		SELECT 
+		SELECT TOP(1)
 			c1.cocode,
 			c1.custmast,
 			c1.custnum,
@@ -98,78 +160,198 @@ func main() {
 
 	stmt, err := db.Prepare(query)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), 120*time.Second)
 	defer cancel()
 	rows, err := stmt.QueryContext(ctx)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	for rows.Next() {
-		var cocode, custmast, taxexempt, po_num,
-			d_waiver, sitename, siteaddr, siteaddr2,
-			sitecity, sitestate, sitezip, sitephone,
-			sitefax, sitezip4, sitecntry, super,
-			billmast, billaddr, billaddr2, billcity,
-			billemail1, billemail2, billfax,
-			billname, billphone, billstate,
-			billzip, billcocode, billcontact,
-			billcountry, acctstatus, accttype,
-			county, custcode1, terms, email, email2 string
-		var custnum, taxpcnt, taxpcnt2, penalty, nomailing, fkjcustmast int
-		var emaillst, nomail bool
-		var startdate time.Time
+		cs := portajohn.CustomerSite{}
+		if err != nil {
+			return err
+		}
+		err = cs.Scan(rows)
+		if err != nil {
+			return err
+		}
 
-		rows.Scan(
-			cocode,
-			custmast,
-			custnum,
-			startdate,
-			taxpcnt,
-			taxpcnt2,
-			taxexempt,
-			penalty,
-			po_num,
-			d_waiver,
-			sitename,
-			siteaddr,
-			siteaddr2,
-			sitecity,
-			sitestate,
-			sitezip,
-			sitephone,
-			sitefax,
-			sitezip4,
-			sitecntry,
-			super,
-			billmast,
-			billaddr,
-			billaddr2,
-			billcity,
-			billemail1,
-			billemail2,
-			billfax,
-			billname,
-			billphone,
-			billstate,
-			billzip,
-			billcocode,
-			billcontact,
-			billcountry,
-			acctstatus,
-			accttype,
-			county,
-			custcode1,
-			terms,
-			email, email2,
-			emaillst,
-			nomail,
-			nomailing,
-			fkjcustmast,
-		)
-		sitephone = portajohn.FormatPhoneNumber(sitephone)
-		billphone = portajohn.FormatPhoneNumber(billphone)
+		err = insertCustomerSiteInfo(cs)
+		if err != nil {
+			return err
+		}
 	}
-}
+	return nil
+} // ./importCustomerSites
+
+// importInventory queries for distinct inventory
+// cleans description, inserts to mongo with unique id
+func importInventory() error {
+	query := `
+		SELECT DISTINCT descrip FROM jivtf01
+	`
+	col := mdb.Collection("products")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	rows, err := db.QueryContext(ctx, query)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		p := portajohn.Product{
+			ID:        primitive.NewObjectID(),
+			Status:    1,
+			CreatedAt: time.Now(),
+			UpdatedAt: time.Now(),
+		}
+		err := rows.Scan(&p.Title)
+		if err != nil {
+			return err
+		}
+		if p.Title == "" {
+			continue
+		}
+		p.Title = portajohn.FormatString(p.Title)
+		_, err = col.InsertOne(ctx, p)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+} // ./importInventory
+
+// importCustomerInventory queries for customer inventory info
+// takes custnum, joins customer table with inventory table on custnum
+func importCustomerInventory(custnum int, customerId, siteId primitive.ObjectID) error {
+	col := mdb.Collection("productlocations")
+	query := `
+		SELECT * FROM jivtf01 AS inv
+		where custnum = @p1
+	`
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	rows, err := db.QueryContext(ctx, query, custnum)
+	if err != nil {
+		return err
+	}
+	for rows.Next() {
+		var inv portajohn.Jinvt
+		err = inv.Scan(rows)
+		if err != nil {
+			return err
+		}
+		p, err := productByTitle(inv.Descrip)
+		if err != nil {
+			return err
+		}
+		pl := portajohn.ProductLocation{
+			ID:           primitive.NewObjectID(),
+			ProductTitle: inv.Descrip,
+			ProductID:    p.ID,
+			CustomerID:   &customerId,
+			SiteID:       &siteId,
+
+			CreatedAt: inv.Inputdate,
+			UpdatedAt: inv.Inputdate,
+		}
+		ctx2, cancel2 := context.WithTimeout(context.Background(), 10*time.Second)
+		_, err = col.InsertOne(ctx2, pl)
+		if err != nil {
+			cancel2()
+			return err
+		}
+		cancel2()
+	}
+	return nil
+} // ./importCustomerInventory
+
+// get product by title
+func productByTitle(title string) (*portajohn.Product, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	filter := bson.M{
+		"title": title,
+	}
+	col := mdb.Collection("products")
+	var p portajohn.Product
+	sr := col.FindOne(ctx, filter)
+	if err := sr.Decode(&p); err != nil {
+		return nil, err
+	}
+	return &p, nil
+} // ./productByTitle
+
+// insertCustomerSiteInfo
+func insertCustomerSiteInfo(cs portajohn.CustomerSite) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	c := cs.Customer()
+	s := cs.Site()
+
+	col := mdb.Collection("useraccounts")
+	colSite := mdb.Collection("sites")
+
+	var cFound portajohn.Customer
+
+	// try search by customer number
+	filter := bson.M{
+		"tacMaster": c.TacMaster,
+	}
+	found := true
+	sr := col.FindOne(ctx, filter)
+	if err := sr.Decode(&cFound); err != nil {
+		if err == mongo.ErrNoDocuments {
+			found = false
+		} else {
+			return err
+		}
+	}
+
+	// no match on cust number
+	// try match by billing address
+	if !found {
+		filter := bson.M{
+			"billingInfo.address": c.BillingInfo.Address,
+		}
+		sr := col.FindOne(ctx, filter)
+		if err := sr.Decode(&cFound); err != nil {
+			if err == mongo.ErrNoDocuments {
+				found = false
+			} else {
+				return err
+			}
+		}
+	}
+
+	// only insert site
+	if found {
+		s.AccountID = cFound.ID
+		s.TacMaster = cFound.TacMaster
+		_, err := colSite.InsertOne(ctx, s)
+		if err != nil {
+			return err
+		}
+		err = importCustomerInventory(cs.Custnum, cFound.ID, s.ID)
+		return err
+	}
+
+	// customer not found
+	// insert site and customer
+	_, err := col.InsertOne(ctx, c)
+	if err != nil {
+		return err
+	}
+	s.AccountID = c.ID
+	s.TacMaster = c.TacMaster
+	_, err = colSite.InsertOne(ctx, s)
+	if err != nil {
+		return err
+	}
+
+	// check inventory
+	err = importCustomerInventory(cs.Custnum, c.ID, s.ID)
+	return err
+} // ./insertCustomerSiteInfo
