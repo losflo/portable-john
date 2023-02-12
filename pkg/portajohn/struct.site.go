3960e14448ac193c9177745a56a7e528713e76b7
losflo <30555738+losflo@users.noreply.github.com>
2023-02-12 13:50:08 -0600

    add fields for metadata

--- pkg/portajohn/struct.site.go
+++ pkg/portajohn/struct.site.go
@@ -14,12 +14,20 @@ type Site struct {
 	PhoneNumber       string             `json:"phoneNumber" bson:"phoneNumber"`
 	Address           string             `json:"address" bson:"address"`
 	City              string             `json:"city" bson:"city"`
-	Location          Location           `json:"location"`
+	Location          *Location          `json:"location,omitempty"`
 	SameAsBillingInfo bool               `json:"isSameAsBillingInfo"`
 	Deleted           bool               `json:"isDeleted"`
 	Status            int                `json:"status" bson:"status"`
-	UID               string             `json:"userId" bson:"userId"`
+	AccountID         primitive.ObjectID `json:"accountId" bson:"accountId"`
 	CreatedBy         primitive.ObjectID `json:"createdBy" bson:"createdBy"`
 	CreatedAt         time.Time          `json:"createdAt" bson:"createdAt"`
 	UpdatedAt         time.Time          `json:"updatedAt" bson:"updatedAt"`
+
+	// added fields
+	Address2    string `json:"address2" bson:"address2"`
+	State       string `json:"state" bson:"state"`
+	Zip         string `json:"zipCode" bson:"zipCode"`
+	Zip4        string `json:"zip4" bson:"zip4"`
+	TacMaster   string `json:"tacMaster" bson:"tacMaster"`
+	TacMasterId int64  `json:"tacMasterId" bson:"tacMasterId"`
 }
